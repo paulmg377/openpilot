7d0f2bdd35eb4708ab1bbfb2ddbeee414481e68f
Justin Newberry <jnewberry0502@gmail.com>
2023-09-30 05:30:13 +0000

    fix that

--- .devcontainer/container_post_start.sh
+++ .devcontainer/container_post_start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # setup safe directories for repo and submodules
-ADD_SAFE=git config --global --add safe.directory
+ADD_SAFE="git config --global --add safe.directory"
 
 $ADD_SAFE $(pwd)
 git submodule foreach "$ADD_SAFE \$(pwd)"
